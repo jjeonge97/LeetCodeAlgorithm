4a2e93b1f14430c342909f32318186f6de0190bd
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-03-13 20:06:40 +0900

    Time: 69 ms (95.88%), Space: 44.8 MB (44.09%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false;
+    
+    let objS = {}
+    let objT = {}
+    
+    for(let el of s) {
+        objS[el] = (objS[el] || 0) + 1
+    }
+    
+    for(let el of t) {
+        objT[el] = (objT[el] || 0) + 1
+    }
+    
+    for(let key in objS) {
+        if(objS[key] !== objT[key]) return false
+    }
+    return true
+};
