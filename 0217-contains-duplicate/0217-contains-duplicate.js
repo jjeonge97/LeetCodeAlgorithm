b84759f4dca5cdf976f71511909311d54917f183
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-03-13 09:57:48 +0900

    Time: 95 ms (71.10%), Space: 56.2 MB (15.10%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,12 +3,13 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let obj = {}
-    nums.map(el => obj[el] ? ++obj[el] : obj[el] = 1)
+//     let obj = {}
+//     nums.map(el => obj[el] ? ++obj[el] : obj[el] = 1)
     
-    for(let key in obj) {
-        if(obj[key] > 1) return true
-    }
-    return false
+//     for(let key in obj) {
+//         if(obj[key] > 1) return true
+//     }
+//     return false
+    let arr = new Set(nums)
+   return [...arr].length === nums.length ? false : true
 };
-    
