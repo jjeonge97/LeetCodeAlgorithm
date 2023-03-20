75417e84a95e2f3d69bfac5de29e9f0005ae29f2
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-03-20 09:12:16 +0900

    Time: 60 ms (76.69%), Space: 42.3 MB (30.22%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -4,6 +4,7 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
+    if(s.length === 0) return true
     let count = 0;
     for(let i = 0; i < t.length; i++) {
         if(t[i] === s[count]) count++
