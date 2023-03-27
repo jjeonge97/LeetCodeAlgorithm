02faa6b85ae8b093c65701d141f76821709c45ad
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-03-27 19:56:29 +0900

    Time: 64 ms (56.17%), Space: 42.1 MB (73.20%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,12 +3,18 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let sum = nums[0]
+//     let sum = nums[0]
+    
+//     for(let i = 1; i < nums.length; i++) {
+//         sum += nums[i]
+//         nums[i] = sum
+//     }
+    
+//     return num
+    
     
     for(let i = 1; i < nums.length; i++) {
-        sum += nums[i]
-        nums[i] = sum
+        nums[i] += nums[i - 1]
     }
-    
-    return nums
+    return nums;
 };
