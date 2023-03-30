3f9658b5d73b15c67d3eec7964258f4af7f66143
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-03-30 23:55:42 +0900

    Time: 66 ms (30.43%), Space: 42.2 MB (34.59%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    
+     // return head.slice(Math.floor(head.length /2))
+    
+    // head = [1,2,3,4,5,6]
+  const arr = [];
+  let length = 0;
+  let list = head;  // list = [1,2,3,4,5,6]
+  while(list) {
+    length++;
+    arr.push(list)
+    list = list.next;
+  }
+  // arr = [ [1,2,3,4,5,6], [2,3,4,5,6], [3,4,5,6], [4,5,6], [5,6], [6] ]
+  return arr[Math.floor(length/2)] // [4,5,6]
+};
+
+// .next : 연결된 목록 노드 개체의 속성 
+// val: 노드 값 보유 / next: 시퀀스의 다음 노드에 대한 참조 보유
