4532c113e415455923ca1668a9d1f3f7e4c36c99
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-04-03 17:44:38 +0900

    Time: 62 ms (100.00%), Space: 43.8 MB (100.00%) - LeetHub

--- 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.js
+++ 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.js
@@ -10,6 +10,12 @@ var minNumber = function(nums1, nums2) {
     let min = 0;
     let result = '';
     
+//     let min = [];
+    
+//     for(let el of nums1) {
+//         if(nums2.includes(el)) min.push(el)
+//     }
+    
     for(let i = 0; i < nums1.length; i++) {
        for(let j = 0; j < nums2.length; j++) {
             if(nums1[i] === nums2[j]) {
@@ -21,12 +27,10 @@ var minNumber = function(nums1, nums2) {
     
     let num1 = Math.min(...nums1)
     let num2 = Math.min(...nums2)
+    
     if(num1 > num2) result = String(num2) + String(num1)
     else result = String(num1) + String(num2)
+    
     return Number(result)
-
     
 };
-// 각 배열에서 적어도 하나의 숫자를 포함하는 가장 작은 숫자를 반환합니다.
-//  nums1 = [3,5,2,6], nums2 = [3,1,7]
-//  nums1 = [2,3,5,6], nums2 = [1,3,7]
