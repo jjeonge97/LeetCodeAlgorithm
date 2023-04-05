1d51b7ce386180187d0be1c16947edefc5b51253
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-04-05 15:44:40 +0900

    Time: 51 ms (94.43%), Space: 42.1 MB (47.40%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    
+    if(nums.includes(target)) {
+        return nums.indexOf(target)
+    }
+    
+    // nums.push(target) 해서 오름차순 정렬 후 target index return
+    
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] >= target) {
+            return i;
+        }
+    }
+
+    return nums.length;
+    
+};
+
+// [1,3,5,6] 7 - 4
+// [1,3,5,6] 0 - 0
