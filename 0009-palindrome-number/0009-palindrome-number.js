c22972cb40e17d1204d04cab745162b6d973e153
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-04-05 09:06:54 +0900

    Time: 164 ms (88.35%), Space: 51.9 MB (15.72%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let str = String(x)
+    let result = ''
+    
+    for(let i = str.length - 1; i >= 0; i--) {
+        result += str[i]
+    }
+    
+    return str === result ? true : false
+};
