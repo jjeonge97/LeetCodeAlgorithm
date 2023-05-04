df4556f8b5deb07b3a78f4cb0a50c6676379de64
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-05-04 09:20:55 +0900

    Time: 95 ms (9.26%), Space: 41.9 MB (52.20%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
 var fib = function(n) {
-    if(n <= 1 ) return n;
     
-    return fib(n-1) + fib(n - 2)
+    if(n <= 1) return n
     
+    return fib(n - 1) + fib(n - 2)
 };
