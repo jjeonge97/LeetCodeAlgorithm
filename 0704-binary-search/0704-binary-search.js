12612a7221175d97d6f51a00483c7c28d7b59447
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-05-16 10:18:41 +0900

    Time: 47 ms (99.33%), Space: 44.4 MB (95.39%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let idx = nums.indexOf(target)
+
+    
+    return idx 
+    
+};
+
+// 오름차순 정렬 배열과 타겟
+// 배열에서 타켓 찾는 함수 
+// 타겟 있으면 타겟의 index. 없으면 -1
