b14e8fc048cc518a579e3079155829c8b2c003c5
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-05-17 10:54:27 +0900

    Time: 115 ms (21.62%), Space: 49.2 MB (17.18%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,7 +3,19 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    return nums.map(el => Math.pow(el,2)).sort((a,b) => a - b)
+    let start = 0;
+    let end = nums.length - 1;
+    let idx = end;
+    let result = []
     
+    while(start <= end) {
+        if(Math.abs(nums[start]) < Math.abs(nums[end])) {
+            result[idx--] = nums[end] * nums[end]
+            end--
+        } else {
+            result[idx--] = nums[start] * nums[start]
+            start++
+        }
+    } 
+    return result
 };
-// 각 숫자의 제곱으로 오름차순 정렬된 배열 리턴
