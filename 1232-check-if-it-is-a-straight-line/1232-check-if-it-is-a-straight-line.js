ca01eddd3a6344f4289e444d701d54a895207ddd
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-06-05 10:23:34 +0900

    Time: 63 ms (62.32%), Space: 44.6 MB (23.19%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+var checkStraightLine = function(coordinates) {
+    if(coordinates.length < 2) return true;
+    
+    let [x0,y0] = coordinates[0];
+    let [x1, y1] = coordinates[1];
+    let dis = (y1 - y0) / (x1 - x0); // 기울기 
+    
+    for(let i = 2; i < coordinates.length; i++) {
+        const [x,y] = coordinates[i];
+        
+        if ((y1 - y0) * (x - x0) !== (y - y0) * (x1 - x0)) return false;
+    }
+    
+    return true;
+    
+};
