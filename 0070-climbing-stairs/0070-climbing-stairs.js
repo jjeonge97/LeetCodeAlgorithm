0c3b6fcde57e4f4d6bd27cd87e94420894c2220d
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-06-05 10:41:33 +0900

    Time: 47 ms (95.09%), Space: 41.7 MB (69.99%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+ let memo = []
+ 
+var climbStairs = function(n) {
+    if(n <= 2) return n;
+    
+    if(memo[n]) return memo[n]
+        
+    memo[n] = climbStairs(n - 1) + climbStairs(n - 2)
+    
+    return memo[n]
+};
