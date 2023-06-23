824135ae309e542681c5d1c55774885fa2a60384
jjeonge97 <110615050+jjeonge97@users.noreply.github.com>
2023-06-23 10:39:58 +0900

    Time: 54 ms (80.84%), Space: 42.1 MB (46.06%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,20 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle)
+    // return haystack.indexOf(needle)
+    
+    const h = haystack.length;
+    const n = needle.length;
+    
+    for(let i = 0; i <= h - n; i++) {
+        let find = true;
+        for(let j = 0; j < n; j++) {
+            if(haystack[i + j] !== needle[j]) {
+                find = false;
+                break;
+            }
+        }
+        if(find) return i
+    }
+    return -1
 };
